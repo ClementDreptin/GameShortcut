d67fac86458081308210339e43dcd034dba6d6a4
Clement <cdreptin@gmail.com>
2021-10-25 21:13:02 +0200

    Made the XEX path configurable via a config file + added an error message box when an error occurs

--- GameShortcut/src/main.cpp
+++ GameShortcut/src/main.cpp
@@ -1,4 +1,7 @@
 #include <xtl.h>
+#include <fstream>
+#include <string>
+#include <vector>
 
 
 struct STRING
@@ -26,13 +29,84 @@ static HRESULT MountHdd()
 }
 
 
+static HRESULT GetGamePath(std::string& strPath)
+{
+    std::ifstream ConfigFile("game:\\config\\gameInfo.txt");
+    if (!ConfigFile.is_open())
+        return E_FAIL;
+
+    std::vector<std::string> lines;
+    lines.reserve(2);
+    std::string strCurrentLine;
+
+    while (std::getline(ConfigFile, strCurrentLine))
+        lines.emplace_back(strCurrentLine);
+
+    if (lines.size() != 2)
+        return E_FAIL;
+
+    strPath = lines[1];
+
+    return S_OK;
+}
+
+
+static HRESULT ShowMessageBoxError(CONST std::string& strMessage)
+{
+    XOVERLAPPED overlapped;
+    ZeroMemory(&overlapped, sizeof(XOVERLAPPED));
+
+    MESSAGEBOX_RESULT result;
+    ZeroMemory(&result, sizeof(MESSAGEBOX_RESULT));
+
+    std::wstring wstrMessage;
+    wstrMessage.assign(strMessage.begin(), strMessage.end());
+
+    LPCWSTR pwstrButtons[] = { L"OK", L"Cancel" };
+
+    DWORD dwResult = XShowMessageBoxUI(
+        0,
+        L"Error",
+        wstrMessage.c_str(),
+        ARRAYSIZE(pwstrButtons),
+        pwstrButtons,
+        0,
+        XMB_ERRORICON,
+        &result,
+        &overlapped
+    );
+
+    if (dwResult != ERROR_IO_PENDING)
+        return E_FAIL;
+
+    while (!XHasOverlappedIoCompleted(&overlapped))
+        Sleep(100);
+
+    dwResult = XGetOverlappedResult(&overlapped, NULL, TRUE);
+
+    if (dwResult == ERROR_ACCESS_DENIED)
+        return E_FAIL;
+
+    return S_OK;
+}
+
+
 int __cdecl main()
 {
     HRESULT hr = MountHdd();
     if (FAILED(hr))
+    {
+        ShowMessageBoxError("Could not mount HDD1.");
         return EXIT_FAILURE;
+    }
 
-    XLaunchNewImage("hdd:\\Games\\Call of Duty 4\\default_mp.xex", NULL);
+    std::string strGamePath;
+    hr = GetGamePath(strGamePath);
+    if (FAILED(hr))
+    {
+        ShowMessageBoxError("The game information file (config\\gameInfo.txt) could not be loaded or has a wrong format.");
+        return EXIT_FAILURE;
+    }
 
-    return EXIT_SUCCESS;
+    XLaunchNewImage(strGamePath.c_str(), NULL);
 }
