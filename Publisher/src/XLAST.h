e0302441ca3f241ad0c2b0f855b383708ff0a671
Clement <cdreptin@gmail.com>
2022-08-11 00:49:48 +0200

    Converted BuildXLASTFile to plain C

--- Publisher/src/XLAST.h
+++ Publisher/src/XLAST.h
@@ -1,71 +1,73 @@
 #pragma once
 
-#include <iostream>
-#include <random>
-#include <algorithm>
-#include <fstream>
-#include <codecvt>
-
 #include <Windows.h>
 
 #include "IO.h"
 #include "Log.h"
 
-HRESULT BuildXLASTFile(const std::string &strGameName)
+HRESULT BuildXLASTFile(const char *szGameName)
 {
-    std::wstring wstrGameName;
-    wstrGameName.assign(strGameName.begin(), strGameName.end());
+    wchar_t wszGameName[50] = { 0 };
+    mbstowcs_s(NULL, wszGameName, 50, szGameName, _TRUNCATE);
 
-    std::random_device RandomDevice;
-    std::mt19937 rng(RandomDevice());
-    std::uniform_int_distribution<std::mt19937::result_type> RandomDistance(0, MAXDWORD);
-    size_t nRandomNumber = RandomDistance(rng);
+    uint32_t nRandomNumber = 0x12345678;
 
-    wchar_t wszBuffer[9] = { 0 };
-    _snwprintf_s(wszBuffer, 9, 9, L"%08x", nRandomNumber);
+    wchar_t wszRandomNumber[9] = { 0 };
+    _snwprintf_s(wszRandomNumber, 9, 9, L"%08x", nRandomNumber);
 
-    std::wstring wstrRandomNumberAsHex(wszBuffer);
-    std::transform(wstrRandomNumberAsHex.begin(), wstrRandomNumberAsHex.end(), wstrRandomNumberAsHex.begin(), towupper);
+    for (size_t i = 0; i < 9; i++)
+        wszRandomNumber[i] = towlower(wszRandomNumber[i]);
 
-    // clang-format off
-    std::wstring wstrFileContent =
+    wchar_t *wszFileContent = (wchar_t *)malloc(2048 * sizeof(wchar_t));
+    if (wszFileContent == NULL)
+        return E_FAIL;
+
+    const wchar_t *wszFileContentFormat =
         L"<?xml version=\"1.0\" encoding=\"UTF-16\" standalone=\"no\"?>\n"
         L"<XboxLiveSubmissionProject Version=\"2.0.21256.0\">\n"
-        L"	<ContentProject clsid=\"{AED6156D-A870-4FF7-924F-F375495A222A}\" Name=\"" + wstrGameName + L"\" TitleID=\"" + wstrRandomNumberAsHex + L"\" TitleName=\"" + wstrGameName + L"\" ActivationDate=\"10/26/2021\" PubOfferingID=\"FFFFFFF\" PubBitFlags=\"FFFFFFFF\" HasCost=\"false\" IsMarketplace=\"true\" AllowProfileTransfer=\"true\" AllowDeviceTransfer=\"true\" DashIconPath=\".\\resources\\icon.png\" TitleIconPath=\".\\resources\\icon.png\" ContentType=\"0x00080000\">\n"
-        L"		<LanguageSettings clsid=\"{F5BA1EE2-D217-447C-93C7-3C7AA6F25DD5}\">\n"
-        L"			<Language clsid=\"{6424EAA3-FA00-4B6C-8CFB-BF063FC18845}\" DashDisplayName=\"" + wstrGameName + L"\" Description=\"" + wstrGameName + L"\"/>\n"
-        L"		</LanguageSettings>\n"
-        L"		<Contents clsid=\"{0D8B38B9-F5A8-4050-8F8D-81F67E3B2456}\">\n"
-        L"			<Folder clsid=\"{0D8B38B9-F5A8-4050-8F8D-81F67E3B2456}\" TargetName=\"config\">\n"
-        L"				<File clsid=\"{8A0BC3DD-B402-4080-8E34-C22144FC1ECE}\" SourceName=\".\\gameInfo.txt\" TargetName=\"gameInfo.txt\"/>\n"
-        L"			</Folder>\n"
-        L"			<File clsid=\"{8A0BC3DD-B402-4080-8E34-C22144FC1ECE}\" SourceName=\".\\default.xex\" TargetName=\"default.xex\"/>\n"
-        L"		</Contents>\n"
-        L"		<ContentOffers clsid=\"{146485F0-DCD7-4AB1-97E1-9B0E64150499}\"/>\n"
-        L"	</ContentProject>\n"
+        L"    <ContentProject clsid=\"{AED6156D-A870-4FF7-924F-F375495A222A}\" Name=\"%s\" TitleID=\"%s\" TitleName=\"%s\" ActivationDate=\"10/26/2021\" PubOfferingID=\"FFFFFFF\" PubBitFlags=\"FFFFFFFF\" HasCost=\"false\" IsMarketplace=\"true\" AllowProfileTransfer=\"true\" AllowDeviceTransfer=\"true\" DashIconPath=\".\\resources\\icon.png\" TitleIconPath=\".\\resources\\icon.png\" ContentType=\"0x00080000\">\n"
+        L"        <LanguageSettings clsid=\"{F5BA1EE2-D217-447C-93C7-3C7AA6F25DD5}\">\n"
+        L"            <Language clsid=\"{6424EAA3-FA00-4B6C-8CFB-BF063FC18845}\" DashDisplayName=\"%s\" Description=\"%s\"/>\n"
+        L"        </LanguageSettings>\n"
+        L"        <Contents clsid=\"{0D8B38B9-F5A8-4050-8F8D-81F67E3B2456}\">\n"
+        L"            <Folder clsid=\"{0D8B38B9-F5A8-4050-8F8D-81F67E3B2456}\" TargetName=\"config\">\n"
+        L"                <File clsid=\"{8A0BC3DD-B402-4080-8E34-C22144FC1ECE}\" SourceName=\".\\gameInfo.txt\" TargetName=\"gameInfo.txt\"/>\n"
+        L"            </Folder>\n"
+        L"            <File clsid=\"{8A0BC3DD-B402-4080-8E34-C22144FC1ECE}\" SourceName=\".\\default.xex\" TargetName=\"default.xex\"/>\n"
+        L"        </Contents>\n"
+        L"        <ContentOffers clsid=\"{146485F0-DCD7-4AB1-97E1-9B0E64150499}\"/>\n"
+        L"    </ContentProject>\n"
         L"</XboxLiveSubmissionProject>";
-    // clang-format on
 
-    char szExecDirBuffer[MAX_PATH] = { 0 };
-    HRESULT hr = GetExecDir(szExecDirBuffer, MAX_PATH);
+    _snwprintf_s(wszFileContent, 2048, 2048, wszFileContentFormat, wszGameName, wszRandomNumber, wszGameName, wszGameName, wszGameName);
+
+    char szFilePath[MAX_PATH] = { 0 };
+    HRESULT hr = GetExecDir(szFilePath, MAX_PATH);
     if (FAILED(hr))
         return E_FAIL;
 
-    std::wofstream XLASTFile(std::string(szExecDirBuffer) + "\\tmp.xlast", std::ios::binary);
-    const std::codecvt_mode mode = static_cast<std::codecvt_mode>(std::generate_header | std::little_endian);
-    std::locale UTF16Locale(XLASTFile.getloc(), new std::codecvt_utf16<wchar_t, 0x10FFFF, mode>);
+    strncat_s(szFilePath, MAX_PATH, "\\tmp.xlast", _TRUNCATE);
 
-    if (!XLASTFile.is_open())
+    FILE *pFile = NULL;
+    fopen_s(&pFile, szFilePath, "w+, ccs=UTF-16LE");
+    if (pFile == NULL)
         return E_FAIL;
 
-    XLASTFile.imbue(UTF16Locale);
+    size_t nWCharCount = wcsnlen_s(wszFileContent, 2048);
+    size_t nWritten = fwrite(wszFileContent, sizeof(wchar_t), nWCharCount, pFile);
 
-    XLASTFile << wstrFileContent;
+    if (nWritten != nWCharCount)
+    {
+        fclose(pFile);
+        return E_FAIL;
+    }
 
-    XLASTFile.close();
+    fclose(pFile);
+    free(wszFileContent);
 
-    std::wstring wstrSuccessMessage = L"XLAST file successfully generated (ID: " + wstrRandomNumberAsHex + L")";
-    LogInfoW(wstrSuccessMessage.c_str());
+    wchar_t wszSuccessMessage[50] = { 0 };
+    _snwprintf_s(wszSuccessMessage, 50, 50, L"XLAST file successfully generated (ID: %s)", wszRandomNumber);
+    LogInfoW(wszSuccessMessage);
 
     return S_OK;
 }
@@ -101,7 +103,7 @@ HRESULT ExecBLAST(const char *szXDKPath)
     WaitForSingleObject(ShExecInfo.hProcess, INFINITE);
     CloseHandle(ShExecInfo.hProcess);
 
-    if (reinterpret_cast<int>(ShExecInfo.hInstApp) <= 32)
+    if ((int)ShExecInfo.hInstApp <= 32)
     {
         DWORD dwError = GetLastError();
         char szErrorMsg[200] = { 0 };
