969501b28c8fd70d55e1cc77269c96e3fc22d515
Clément Dreptin <cdreptin@gmail.com>
2022-10-12 00:49:02 +0200

    Stopped using system pauses before the program ends

--- Publisher/src/main.c
+++ Publisher/src/main.c
@@ -7,43 +7,28 @@ int main()
 
     hr = AddXdkBinDirToPath();
     if (FAILED(hr))
-    {
-        system("pause");
         return EXIT_FAILURE;
-    }
 
     hr = CheckXbdmConnection();
     if (FAILED(hr))
-    {
-        system("pause");
         return EXIT_FAILURE;
-    }
 
     hr = GetShortcutName(shortcutName, SHORCUT_NAME_LENGTH);
     if (FAILED(hr))
-    {
-        system("pause");
         return EXIT_FAILURE;
-    }
 
     hr = BuildXLastFile(shortcutName);
     if (FAILED(hr))
-    {
-        system("pause");
         return EXIT_FAILURE;
-    }
 
     hr = ExecBlast();
     if (FAILED(hr))
     {
         Cleanup();
-        system("pause");
         return EXIT_FAILURE;
     }
 
     Cleanup();
-
-    system("pause");
 
     return EXIT_SUCCESS;
 }
